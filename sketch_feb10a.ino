b375f9e1bcd2479bbffca037dac18925f54aa8bb
iFatRain <thejesusfreak911@yahoo.com>
2019-09-17 19:34:50 -0500

    Accompaning file for serial and actuator motion

--- sketch_feb10a.ino
+++ sketch_feb10a.ino
@@ -1,29 +1,59 @@
-/*
-  ReadAnalogVoltage
-  Reads an analog input on pin 0, converts it to voltage, and prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-  This example code is in the public domain.
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
 */
-// the setup routine runs once when you press reset:
+
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 90;    // variable to store the servo position
+int wind = 0;
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-    Serial.println(voltage);
 
-
-    if (Serial.read() == 'l')
-    digitalWrite(LED_BUILTIN, HIGH);   
+  if (Serial.read() == 'l'){
+    digitalWrite(LED_BUILTIN, HIGH);
+    if(pos == 60)
+      wind = 0;
+    if(pos == 120)
+      wind = 1;
+      if (wind == 0){
+        for (int i=0; i <= 5; pos++){
+      if(pos == 120)
+      wind = 1;
+      myservo.write(pos);      
+      i++;// tell servo to go to position in variable 'pos'
+      delay(15);
+      }
+    }else if (wind == 1){
+      
+      for (int i=0; i <= 5; pos--){
+      myservo.write(pos);
+      if(pos == 60)
+      wind = 0;   
+      i++;// tell servo to go to position in variable 'pos'
+      delay(15);
+      } 
+      // waits 15ms for the servo to reach the position
+  }
+  /*
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);             // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }*/
+  }
   else digitalWrite(LED_BUILTIN, LOW);
-
+  
+  
 }
