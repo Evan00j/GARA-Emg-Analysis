c27529ab1c459fd4a9add1e8ee95bf52d8e54d86
iFatRain <thejesusfreak911@yahoo.com>
2019-10-02 21:03:52 -0500

    Working communication
    Paired with commit of similar date/time, the python end of the arduino communication which is able to read and send keywords to execute tasks

--- EMG_Scripts/Realtime_EMG.py
+++ EMG_Scripts/Realtime_EMG.py
@@ -33,8 +33,25 @@ def serBurstReadTest(burst_length):
     return readbuffer
 
 ## Setup for serial read/write
-##ser_line = connectToCOM('COM9', 9600)
+ser_line = connectToCOM('COM3', 9600)
+
+print('Listening')
 
 while (True):
-    testList = serBurstReadTest(2)
-    print(len(testList), ' ', testList, '\n')
+##    testList = serBurstReadTest(2)
+##    print(len(testList), ' ', testList, '\n')
+
+    ser_line.write('changeGrip'.encode())
+    byte = ser_line.readline()
+    mot = byte.decode('utf-8')
+    if mot[:-2] != '\n':
+        print(mot[:-2])
+
+    if mot[:-2] == 'Moving':
+        print('Waiting for movement!')
+        while True:
+            byte = ser_line.readline()
+            mot = byte.decode('utf-8')
+            if mot[:-2] == 'Done':
+                print(mot[:-2])
+                break
