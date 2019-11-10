a00c235b45227f8d4d544ddd1f9a057b0b24ba35
Evan00j <jenkins00evan@gmail.com>
2019-11-10 16:56:06 -0600

    updated Classification3Emgv2

--- Classification3Emgv2.py
+++ Classification3Emgv2.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[329]:
-
 
 import numpy as np
 import pandas as pd
@@ -26,10 +22,6 @@ f = open(file_to_open)
 
 print(f.read())
 
-
-# In[331]:
-
-
 f = open("ARest.txt","w+")
 f1 = open("BRest.txt", "w+")
 f2 = open("CRest.txt", "w+")
@@ -43,15 +35,9 @@ for lines in fileinput.FileInput('FastBaudEMG/TripleREST.txt', inplace=1):
     print(lines)
 
 
-# In[332]:
-
-
 Arest = np.loadtxt('ARest.txt')
 Brest = np.loadtxt('BRest.txt')
 Crest = np.loadtxt('CRest.txt')
-
-
-# In[333]:
 
 
 #Calculate RestAvg
@@ -59,9 +45,6 @@ Arestavg = np.average(Arest)
 Brestavg = np.average(Brest)
 Crestavg = np.average(Crest)
 Arestavg
-
-
-# In[334]:
 
 
 def processText(Aname, Bname, Cname, filein):
@@ -89,91 +72,42 @@ def processText(Aname, Bname, Cname, filein):
     return Aname, Bname, Cname
 
 
-# ## Process and Strip Triple Index Hard into ATIH, BTIH and CTIH
-
-# In[335]:
+# Process and Strip Triple Index Hard into ATIH, BTIH and CTIH
 
 
 ATIH, BTIH, CTIH = processText(ATIH, BTIH, CTIH, "FastBaudEMG/TripleIndexHard1.txt")
 
 
-# In[336]:
+#  Process and Strip Thumb Hard into ATTH, BTTH and CTTH
 
-
-out = emg.emg(signal=ATIH, sampling_rate=(500), show=True)
-
-
-# ## Process and Strip Thumb Hard into ATTH, BTTH and CTTH
-
-# In[337]:
 
 
 ATTH, BTTH, CTTH = processText(ATTH, BTTH, CTTH, "FastBaudEMG/TripleThumbHard2.txt")
 
 
-# In[338]:
 
-
-out = emg.emg(signal=ATTH, sampling_rate=(250), show=True)
-
-
-# # Process and Strip Ring Middle Hard into ATRMH, BTRMH and CTRMH
-
-# In[358]:
+#  Process and Strip Ring Middle Hard into ATRMH, BTRMH and CTRMH
 
 
 ATRMH, BTRMH, CTRMH = processText(ATRMH, BTRMH, CTRMH, "FastBaudEMG/TripleRMHard1.txt")
 
 
-# In[340]:
-
-
-out = emg.emg(signal=BTRMH, sampling_rate=(250), show=True)
-
-
-# # Process and Strip Ring Middle Soft into ATRMS, BTRMS and CTRMS
-# ## Use 1 or 3
-
-# In[341]:
+#  Process and Strip Ring Middle Soft into ATRMS, BTRMS and CTRMS
+#  Use 1 or 3
 
 
 ATRMS, BTRMS, CTRMS = processText(ATRMS, BTRMS, CTRMS, "FastBaudEMG/TripleRMSoft2.txt")
 
 
-# In[342]:
-
-
-out = emg.emg(signal=BTRMS, sampling_rate=(250), show=True)
-
-
 # # Process and Strip Open Wrist Flex into AOWF, BOWF and COWF
 
-# In[343]:
-
-
 ATOWF, BTOWF, CTOWF = processText(ATOWF, BTOWF, CTOWF, "FastBaudEMG/TripleOpenWristFlex2.txt")
-
-
-# In[344]:
-
-
-out = emg.emg(signal=CTOWF, sampling_rate=(250), show=True)
-
-
-# In[345]:
 
 
 classvar = np.zeros(len(ATIH), dtype=int)
 classvar
 
-
-# In[346]:
-
-
-CTIH.size
-
-
-# # Start with Triple Index Hard
+#  Start with Triple Index Hard
 
 # In[347]:
 
@@ -181,9 +115,7 @@ CTIH.size
 dfTIH = pd.DataFrame({'AData':ATIH, 'BData':BTIH, 'CData':CTIH, 'ATrigger':classvar, 'BTrigger':classvar, 'CTrigger':classvar, 'MAVA':classvar, 'MAVB':classvar, 'MAVC':classvar, 'MAVSA':classvar, 'MAVSB':classvar, 'MAVSC':classvar,'SSIA':classvar, 'SSIB':classvar, 'SSIC':classvar, 'VARA':classvar, 'VARB':classvar, 'VARC':classvar,'RMSA':classvar, 'RMSB':classvar, 'RMSC':classvar, 'WLA':classvar, 'WLB':classvar, 'WLC':classvar, 'Trigger':classvar})
 
 
-# # We want to determine our Onset on EMG A since it is what triggers the hardest
-
-# In[348]:
+# We want to determine our Onset on EMG A since it is what triggers the hardest
 
 
 for index, row in dfTIH.iterrows():
@@ -194,8 +126,6 @@ for index, row in dfTIH.iterrows():
     if row['CData'] > (.8+Crestavg):
         dfTIH.loc[index, 'CTrigger'] = 1
 
-
-# In[349]:
 
 
 def claculateDF(df, MT, Tval):
@@ -326,29 +256,16 @@ def claculateDF(df, MT, Tval):
                 df.loc[index+index2, 'Trigger'] = 0
 
 
-# In[350]:
-
-
 claculateDF(dfTIH, "ATrigger", 1)
 
 
-# # Now Triple Thumb Hard
-
-# In[351]:
-
+#  Now Triple Thumb Hard
 
 classvar = np.zeros(len(ATTH), dtype=int)
 classvar
 
 
-# In[352]:
-
-
 dfTTH = pd.DataFrame({'AData':ATTH, 'BData':BTTH, 'CData':CTTH, 'ATrigger':classvar, 'BTrigger':classvar, 'CTrigger':classvar, 'MAVA':classvar, 'MAVB':classvar, 'MAVC':classvar, 'MAVSA':classvar, 'MAVSB':classvar, 'MAVSC':classvar,'SSIA':classvar, 'SSIB':classvar, 'SSIC':classvar, 'VARA':classvar, 'VARB':classvar, 'VARC':classvar,'RMSA':classvar, 'RMSB':classvar, 'RMSC':classvar, 'WLA':classvar, 'WLB':classvar, 'WLC':classvar, 'Trigger':classvar})
-
-
-# In[353]:
-
 
 for index, row in dfTTH.iterrows():
     if row['AData'] > (.8+Arestavg):
@@ -359,34 +276,16 @@ for index, row in dfTTH.iterrows():
         dfTTH.loc[index, 'CTrigger'] = 1
 
 
-# In[354]:
-
-
 claculateDF(dfTTH, "ATrigger", 2)
 
 
-# In[355]:
-
-
-dfTTH.head(1000)
-
-
-# # Ring Middle Hard
-
-# In[356]:
-
+#  Ring Middle Hard
 
 classvar = np.zeros(len(BTRMH), dtype=int)
 classvar
 
 
-# In[359]:
-
-
 dfTRMH = pd.DataFrame({'AData':ATRMH, 'BData':BTRMH, 'CData':CTRMH, 'ATrigger':classvar, 'BTrigger':classvar, 'CTrigger':classvar, 'MAVA':classvar, 'MAVB':classvar, 'MAVC':classvar, 'MAVSA':classvar, 'MAVSB':classvar, 'MAVSC':classvar,'SSIA':classvar, 'SSIB':classvar, 'SSIC':classvar, 'VARA':classvar, 'VARB':classvar, 'VARC':classvar,'RMSA':classvar, 'RMSB':classvar, 'RMSC':classvar, 'WLA':classvar, 'WLB':classvar, 'WLC':classvar, 'Trigger':classvar})
-
-
-# In[360]:
 
 
 for index, row in dfTRMH.iterrows():
@@ -398,34 +297,18 @@ for index, row in dfTRMH.iterrows():
         dfTRMH.loc[index, 'CTrigger'] = 1
 
 
-# In[361]:
-
-
 claculateDF(dfTRMH, "BTrigger", 3)
 
 
-# In[362]:
-
-
-dfTRMH.head(1000)
-
 
 # # Ring Middle Soft
-
-# In[363]:
 
 
 classvar = np.zeros(len(ATRMS), dtype=int)
 classvar
 
 
-# In[364]:
-
-
 dfTRMS = pd.DataFrame({'AData':ATRMS, 'BData':BTRMS, 'CData':CTRMS, 'ATrigger':classvar, 'BTrigger':classvar, 'CTrigger':classvar, 'MAVA':classvar, 'MAVB':classvar, 'MAVC':classvar, 'MAVSA':classvar, 'MAVSB':classvar, 'MAVSC':classvar,'SSIA':classvar, 'SSIB':classvar, 'SSIC':classvar, 'VARA':classvar, 'VARB':classvar, 'VARC':classvar,'RMSA':classvar, 'RMSB':classvar, 'RMSC':classvar, 'WLA':classvar, 'WLB':classvar, 'WLC':classvar, 'Trigger':classvar})
-
-
-# In[365]:
 
 
 for index, row in dfTRMS.iterrows():
@@ -437,34 +320,16 @@ for index, row in dfTRMS.iterrows():
         dfTRMS.loc[index, 'CTrigger'] = 1
 
 
-# In[366]:
-
-
 claculateDF(dfTRMS, "BTrigger", 4)
 
 
-# In[367]:
-
-
-dfTRMS.head(1000)
-
-
 # # Open Wrist Flex
-
-# In[368]:
-
 
 classvar = np.zeros(len(ATOWF), dtype=int)
 classvar
 
 
-# In[369]:
-
-
 dfTOWF = pd.DataFrame({'AData':ATOWF, 'BData':BTOWF, 'CData':CTOWF, 'ATrigger':classvar, 'BTrigger':classvar, 'CTrigger':classvar, 'MAVA':classvar, 'MAVB':classvar, 'MAVC':classvar, 'MAVSA':classvar, 'MAVSB':classvar, 'MAVSC':classvar,'SSIA':classvar, 'SSIB':classvar, 'SSIC':classvar, 'VARA':classvar, 'VARB':classvar, 'VARC':classvar,'RMSA':classvar, 'RMSB':classvar, 'RMSC':classvar, 'WLA':classvar, 'WLB':classvar, 'WLC':classvar, 'Trigger':classvar})
-
-
-# In[370]:
 
 
 for index, row in dfTOWF.iterrows():
@@ -476,30 +341,15 @@ for index, row in dfTOWF.iterrows():
         dfTOWF.loc[index, 'CTrigger'] = 1
 
 
-# In[371]:
-
-
 claculateDF(dfTOWF, "CTrigger", 5)
 
 
-# In[372]:
-
-
-dfTOWF.head(1000)
-
-
 # # Split Apart Train and Test
-
-# In[373]:
-
 
 def TrainTest(df):
     train = df.loc[0:len(df.index)*.75]
     test = df.loc[len(train.index):len(df.index)]
     return train, test
-
-
-# In[374]:
 
 
 trainTIH, testTIH = TrainTest(dfTIH)
@@ -508,63 +358,24 @@ trainTRMH, testTRMH = TrainTest(dfTRMH)
 trainTRMS, testTRMS = TrainTest(dfTRMS)
 trainTOWF, testTOWF = TrainTest(dfTOWF)
 
-
-# In[375]:
-
-
-print(dfTIH.index.size, trainTIH.index.size, testTIH.index.size)
-print(dfTTH.index.size, trainTTH.index.size, testTTH.index.size)
-print(dfTRMH.index.size, trainTRMH.index.size, testTRMH.index.size)
-print(dfTRMS.index.size, trainTRMS.index.size, testTRMS.index.size)
-print(dfTOWF.index.size, trainTOWF.index.size, testTOWF.index.size)
-
-
-# In[376]:
-
-
 traintot = pd.concat([trainTIH, trainTOWF, trainTRMH, trainTRMS, trainTTH])
 traintot.index.size
-
-
-# In[377]:
 
 
 testtot = pd.concat([testTIH, testTOWF, testTRMH, testTRMS, testTTH])
 testtot.index.size
 
 
-# In[378]:
-
-
 target = traintot.pop('Trigger')
-
-
-# In[379]:
-
-
-target
-
-
-# In[380]:
 
 
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(traintot, target)
 
-
-# In[381]:
-
-
 answers = testtot.pop("Trigger")
 
 
-# In[382]:
-
-
 clf.score(testtot, answers)
-
-
-# In[383]:
 
 
 predictions = clf.predict(testtot)
@@ -572,51 +383,15 @@ predictions = np.asarray(predictions)
 predictions = pd.DataFrame(predictions)
 
 
-# In[384]:
-
-
 answersdf = pd.DataFrame(answers)
 answersdf = answersdf.reset_index()
 del answersdf['index']
 
 
-# In[385]:
-
-
-compare = pd.concat([predictions, answersdf], axis=1, sort=False)
-with pd.option_context("display.max_rows", None):
-    display(compare)
-
-
-# In[386]:
-
-
 print(dict(zip(traintot.columns, clf.feature_importances_)))
-
-
-# In[387]:
 
 
 from joblib import dump, load
 dump(clf, '3emg.joblib') 
 clf2 = load('3emg.joblib') 
 clf2.score(testtot, answers)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
